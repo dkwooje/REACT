60a6de787c0ac0bea3f2099e5deb3764b18b5d0f
wooje <dkwooje@naver.com>
2024-08-23 17:23:22 +0900

    t

--- 2/03.JOCJEABI/src/assets/components/GameBoard.jsx
+++ 2/03.JOCJEABI/src/assets/components/GameBoard.jsx
@@ -1,29 +1,23 @@
-import { useState } from "react"
-const initialGameBoard = [
-    [null,null,null],  //initialGameBoard의 0번 인덱스
-    [null,null,null],
-    [null,null,null]
-]
-export default function GameBoard({onSelectSqure, activePlayerSymbol}){
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex,colIndex){
-        setGameBoard((prevGameBoard)=>{
-            const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-
-        onSelectSqure();
-
-    }
-    return(
-            <ol id="game-board">
-                {gameBoard.map((row, rowIndex)=> (<li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button></li>)}
-                    </ol>
-                 </li>))}
-            </ol>
-    )
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
--- 2/03.JOCJEABI/src/assets/components/Player.jsx
+++ 2/03.JOCJEABI/src/assets/components/Player.jsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
 
-export default function Player({initialName, symbol, isActive}){
-    const [playerName,setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-    setIsEditing((editing)=> !editing);  //!isEditing은 느리다
-    }
+  function handleEditClick() {
+    setIsEditing((editing) => {
+      const newEditingState = !editing;
+      if (!newEditingState) {
+        onChangeName(symbol, playerName);
+      }
+      return newEditingState;
+    });
+  }
 
-    function handleChange(event){
-        console.log(event);
-        setPlayerName(event.target.value);
-    }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    let editableplayerName =  <span className="player-name">{playerName}</span>
-    if(isEditing){
-        editableplayerName = <input type= "text" required value={playerName} onChange={handleChange}/>;
-    }
-    return (
-      <li className="{isActive ? 'active' : undefined}">
-        <span className="player">
-          {editableplayerName}
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleEditClick}>{ isEditing ? '저장' : '편집'}</button>
-      </li>
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "저장" : "편집"}</button>
+    </li>
+  );
 }
